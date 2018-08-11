784711f1e69418664bb4963c5846c43d6646e64e
Ignacio Baca Moreno-Torres <ignacio@bacamt.com>
2018-08-11 09:37:31 +0200

    Add HomePresenter basic calculator as an events combination using rx

--- sample/src/main/java/sample/nested/client/application/HomePresenter.java
+++ sample/src/main/java/sample/nested/client/application/HomePresenter.java
@@ -1,13 +1,21 @@
 package sample.nested.client.application;
 
+import static com.intendia.rxgwt2.user.RxUser.bindValueChangeOr;
+
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiTemplate;
+import com.google.gwt.user.client.ui.InlineLabel;
+import com.google.gwt.user.client.ui.IntegerBox;
 import com.google.gwt.user.client.ui.Widget;
 import com.intendia.reactivity.client.CompositeView;
 import com.intendia.reactivity.client.Place;
 import com.intendia.reactivity.client.PlaceRequest;
 import com.intendia.reactivity.client.PresenterChild;
+import com.intendia.rxgwt2.user.RxHandlers;
+import io.reactivex.Observable;
 import io.reactivex.Single;
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -27,8 +35,19 @@ public class HomePresenter extends PresenterChild<HomePresenter.MyView> {
             Ui binder = GWT.create(Ui.class);
         }
 
+        @UiField IntegerBox a;
+        @UiField IntegerBox b;
+        @UiField InlineLabel c;
+
         @Inject MyView() { initWidget(Ui.binder.createAndBindUi(this)); }
     }
 
-    @Inject HomePresenter(MyView view, ApplicationPresenter.MainContent parent) { super(view, parent); }
+    @Inject HomePresenter(MyView view, ApplicationPresenter.MainContent parent) {
+        super(view, parent);
+        onReveal(Observable.combineLatest(bindValueChangeOr(view.a, 0), bindValueChangeOr(view.b, 0), (a, b) -> a + b)
+                .doOnNext(c -> view.c.setText(Integer.toString(c))));
+        // GWT valueBox send only events on blur, so bind keyUp and force change events to get better interactivity
+        onReveal(RxHandlers.keyUp(view.a).doOnNext(ev -> ValueChangeEvent.fire(view.a, view.a.getValue())));
+        onReveal(RxHandlers.keyUp(view.b).doOnNext(ev -> ValueChangeEvent.fire(view.b, view.b.getValue())));
+    }
 }
